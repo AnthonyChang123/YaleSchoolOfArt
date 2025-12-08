6abb5120ab9ebee3f56108df98963fb936df03fa
AnthonyChang123 <fz7416lx@go.minnstate.edu>
2025-12-08 01:22:38 -0600

    Pushes

--- script.js
+++ script.js
@@ -27,10 +27,10 @@ const prevBtn = document.getElementById("prevSlide");
 const nextBtn = document.getElementById("nextSlide");
 
 const slides = [
-  { src: "yale-art-1.jpg", alt: "Yale School of Art 1" },
-  { src: "yale-art-2.jpg", alt: "Yale School of Art 2" },
-  { src: "yale-art-3.webp", alt: "Yale School of Art 3" },
-  { src: "yale-art-4.jpg", alt: "Yale School of Art 4" }
+  { src: "pictures/yale-art-1.jpg", alt: "Yale School of Art 1" },
+  { src: "pictures/yale-art-2.jpg", alt: "Yale School of Art 2" },
+  { src: "pictures/yale-art-3.webp", alt: "Yale School of Art 3" },
+  { src: "pictures/yale-art-4.jpg", alt: "Yale School of Art 4" }
 ];
 
 let currentSlide = 0;
